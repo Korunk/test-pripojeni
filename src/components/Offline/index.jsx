4fd7e9ea2a8ef13504f620392c0f1c9ddd4e4b46
Klára Chmelařová <korunka91@gmail.com>
2022-09-30 11:22:42 +0200

    handleOffline se volá pouze jednou

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -3,9 +3,29 @@ import React, { useEffect, useState } from 'react';
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
 
+  useEffect(
+() => {
+  console.log("mounted")
+  window.addEventListener('online', handleOffline)
+  window.addEventListener('offline', handleOffline)
+  
+  return () => {
+    console.log("unmount")
+    window.removeEventListener('online', handleOffline)
+    window.removeEventListener('offline', handleOffline)
+  }
+}
+  ),[];
+
+  const handleOffline = () => {
+    setIsOnline(false)
+    console.log("Jsi offline")
+  }
+
   if (isOnline) {
     return (
       <p>Všechno v pořádku</p>
+      
     )
   };
 
